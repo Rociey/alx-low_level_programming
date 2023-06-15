37429cb618c68a72e5f81d754c322c55963a48df
Nompe Rose Maduna <Nomperose03@gmail.com>
2023-06-15 05:54:07 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -4,8 +4,8 @@
 
 /**
  * main - prints "Programming is like building a multilingual 
- *   puzzle, followed by a new line." 
- *     Return: Always 0.  
+ * puzzle, followed by a new line.
+ * Return: Always 0.  
  */
 
 
