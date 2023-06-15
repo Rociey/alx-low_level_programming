363aff0aaa2eeda1416390fc21878ee76d9f4d05
Nompe Rose Maduna <Nomperose03@gmail.com>
2023-06-15 06:28:07 -0700

    6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,2 +1,26 @@
-#!/bin/bash
-gcc 6-size.c -m64 -o size64 2> /tmp/64
+#include <stdio.h>
+
+/**
+ * main - prints the size of various typesa based on 
+ * the computer it is compiled and run on..
+ * Return: Always 0.
+ */
+
+
+int main(void)
+
+
+{
+
+
+	printf("Size of a char: %zu byte(s)\n", sizeof(char));
+
+	printf("Size of a int: %zu byte(s)\n", sizeof(int));
+	printf("Size of a long int: %zu byte(s)\n", sizeof(long int));
+	printf("Size of a long long int: %zu byte(s)\n", sizeof(long long int));
+	printf("Size of a float %zu byte(s)\n", sizeof(float));
+	return (0);
+
+
+
+}	
