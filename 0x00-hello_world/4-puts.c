4a84f8cbd027c8ad04d813d6d57b21811b3f7c00
Nompe Rose Maduna <Nomperose03@gmail.com>
2023-06-15 05:48:58 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,19 +1,20 @@
-#!/bin/bash
 #include <stdio.h>
 
+
+
 /**
- * main - prints "Programming is like building a multilingual
- *  puzzle, followed by a new line.
- *  Return: Always 0.
+ * main - prints "Programming is like building a multilingual 
+ *   puzzle, followed by a new line." 
+ *     Return: Always 0.  
  */
+
 
 int main(void)
 
+{    	
+	
+	puts("\"Programming is like building a multilingual puzzle");   
+	return (0);
 
-{
-    
-
-      puts("\"Programming is like building a multilingual puzzle");
-      return (0);
 
 }
