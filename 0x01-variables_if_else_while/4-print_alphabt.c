0dee16a7856047d69ce2bc6305860a8c0243060f
Nompe Rose Maduna <Nomperose03@gmail.com>
2023-06-21 15:05:26 -0700

    4-print_alphabt.c

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -1,27 +1,26 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include <time.h>
 
 /**
  * main - Entry point
- * Return: Always 0 (success)
+ *
+ *
+ * Return: Always 0 (Sucess)
  */
 
 int main(void)
-
 {
-	char low, e, q;
+	char c;
 
-	e = 'e';
-	q = 'q';
+	for (c = 'a' ; c <= 'z' ; c++)
+	{
+		if ((c == 'q' || c == 'e') != 1)
+		{
+			putchar(c);
+		}
 
-	for (low = 'a'; low <= 'z'; low++)
-        {
-	if (low != e && low !=q)
-	putchar(low);
 	}
 	putchar('\n');
 	return (0);
-
 }
+
 
