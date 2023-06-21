1d81a181a9979fb5ca8c81db351312c3cf44706c
Nompe Rose Maduna <Nomperose03@gmail.com>
2023-06-21 14:56:44 -0700

    3-print_alphabets.c

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,6 +1,4 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include <time.h>
 
 /**
  * main - Entry point
@@ -14,10 +12,14 @@ int main(void)
 	char low;
 
 	for (low = 'a'; low <= 'z'; low++)
+	{
 	putchar(low);
+	}
 
         for (low = 'A'; low <= 'Z'; low++)
+	{
 	putchar(low);
+	}
         putchar('\n');
 
         return (0);
