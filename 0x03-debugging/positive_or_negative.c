a46098822267dc9761b3320c586abd5fe90bdc86
Nompe Rose Maduna <Nomperose03@gmail.com>
2023-06-22 14:09:04 -0700

    0-main.c

--- 0x03-debugging/positive_or_negative.c
+++ 0x03-debugging/positive_or_negative.c
@@ -1,8 +1,21 @@
-#ifndef MAIN_H
-#define MAIN_H
-
 #include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
 
-void positive_or_negative(int i);
+/**
+ * positive_or_negative - print a number
+ * @i : integer
+ * if the number is greater than 0: is posituve, if the number is 0: is zero
+ * if the number is less thsn 0: is negative, followed by a new line
+ * Return: 0
+ */
 
-#endif /* MAIN_H */
+void positive_or_negative(int i)
+{
+	if (i > 0)
+		printf("%d is positive\n", i);
+	else if (i < 0)
+		printf("%d is negative\n", i);
+	else
+		printf("%d is zero\n", i);
+}
