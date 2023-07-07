4006790ca9a6cb07b7b72e24b9ac34bb42b12562
Nompe Rose Maduna <Nomperose03@gmail.com>
2023-07-07 15:44:55 -0700

    0-whatsmyname.c

---
+++ 0-whatsmyname.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - prints the name of the program
+ * @argc: number of arguments
+ * @argc: array of arguments
+ *
+ * return: Always 0(success)
+ */
+int main(int argc_attribute_((unused)),char *argv[])
+{
+	print("%s\n*, *argv");
+
+	return (0);
+}
