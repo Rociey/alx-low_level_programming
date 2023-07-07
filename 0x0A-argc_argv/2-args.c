64789bcc6870176f1759fdbb49492a57856d037b
Nompe Rose Maduna <Nomperose03@gmail.com>
2023-07-07 15:48:55 -0700

    2-args.c

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - print all arguments it receives
+ * @argv: number of arguments
+ * @argv: array of arguments
+ *
+ * return: alawys 0 (suscess)
+ */
+int main(int argc, char *argv[])
+{
+        int i;
+        for (i = 0; i < argc; i++)
+
+                {
+                        printf("%s\n", argv[i]);
+
+                }
+        return (0);
+}
